a5cbfa0bd5747c0005739d6b7a48523567368a44
Henry Estberg <henrye1@outlook.com>
2022-02-28 16:29:02 -0800

    setting up initial variables to track enemies

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -1,4 +1,12 @@
+"""
+This file contains all agents to be used in createTeam
+Authors:
+Henry Estberg (henrye1@outlook.com)
+"""
+
 from pacai.util import reflection
+from pacai.agents.capture.capture import CaptureAgent
+from pacai.core.directions import Directions
 
 def createTeam(firstIndex, secondIndex, isRed,
         first = 'pacai.agents.capture.dummy.DummyAgent',
@@ -17,3 +25,72 @@ def createTeam(firstIndex, secondIndex, isRed,
         firstAgent(firstIndex),
         secondAgent(secondIndex),
     ]
+
+# This agent attempts to target the nearest pellet on the other side of the board
+class OffensiveAgent(CaptureAgent):
+
+    # I'm not incredibly sure where the gameState that this is called with is coming from,
+    # so for now we just generate a new one with getCurrentObservation()
+    def chooseAction(self, gameState):
+        # First we look for the enemies that the agent can currently see
+        agentState = self.getCurrentObservation()
+        enemies = [agentState.getAgentState(i) for i in self.getOpponents(agentState)]
+        # Ghosts now contains a list of the enemy ghost agents that this agent can see
+        ghosts = [a for a in enemies if not a.isPacman() and a.getPosition() is not None]
+
+        if len(ghosts) > 0:
+            # ghostDistance is how far away the agent is from all visible ghosts
+            currentPos = agentState.getPosition()
+            ghostDistance = []
+            for g in ghosts:
+                ghostDistance.append(self.getMazeDistance(currentPos, g.getPosition()))
+
+        actions = gameState.getLegalActions(self.index)
+        # I'm assuming we want to avoid the agent just pausing so I am not including stop
+        for action in actions:
+            if action != Directions.STOP:
+                successor = self.getSuccessor(gameState, action)
+                successorPos = successor.getPosition()
+                # newGhostDistance is a list of the new distance between the agent and any
+                # visible ghosts, in their old positions, if pacman makes the given action
+                if len(ghosts) > 0:
+                    newGhostDistance = []
+                    distanceChange = []
+                    for g in ghosts:
+                        newGhostDistance.append(self.getMazeDistance(successorPos, g.getPosition()))
+                    # distanceChange shows how much closer/farther pacman gets to every ghost with
+                    # the given action
+                    for d, nd in ghostDistance, newGhostDistance:
+                        distanceChange.append(nd - d)
+
+
+class DefensiveAgent(CaptureAgent):
+    # I'm not incredibly sure where the gameState that this is called with is coming from,
+    # so for now we just generate a new one with getCurrentObservation()
+    def chooseAction(self, gameState):
+        # First we look for the enemies that the agent can currently see
+        agentState = self.getCurrentObservation()
+        enemies = [agentState.getAgentState(i) for i in self.getOpponents(agentState)]
+        # Invaders now contins a list of the enemy pacman agents that this agent can see
+        invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
+
+        # closestInvader is the pacman that is closest to the agent
+        currentPos = agentState.getPosition()
+        if len(invaders) > 0:
+            closestInvader = invaders[0]
+            shortestDistance = self.getMazeDistance(currentPos, invaders[0].getPosition())
+            for i in invaders:
+                distance = self.getMazeDistance(currentPos, i.getPosition())
+                if distance < shortestDistance:
+                    shortestDistance = distance
+                    closestInvader = i
+
+        actions = gameState.getLegalActions(self.index)
+        # I'm assuming we want to avoid the agent just pausing so I am not including stop
+        for action in actions:
+            if action != Directions.STOP:
+                successor = self.getSuccessor(gameState, action)
+                successorPos = successor.getPosition()
+                if len(invaders) > 0:
+                    newDistanceToInvader = self.getMazeDistance(successorPos,
+                                                                closestInvader.getPosition())
